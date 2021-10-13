b8ce6907a622604270c1eabc3a54b041ff24f196
HoangZoan <d.hoang1906@gmail.com>
2021-10-13 21:57:03 +0700

    Add JS script, progressing responsive for mobile media

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 // Testimonial hover effect
-
 const rightColEL = document.querySelector(".testimonials__right-col");
 const cardsEl = document.querySelectorAll(".testimonials__card");
 const imagesEl = document.querySelectorAll(".testimonials__img");
@@ -20,4 +19,25 @@ rightColEL.addEventListener("mouseover", function (e) {
 
     img.classList.remove("hidden");
   });
+});
+
+// Burger menu popup
+const burgerBtnEl = document.querySelector(".burger-btn");
+const shadeLayoutEl = document.querySelector(".burger-layout");
+const burgerMenuEl = document.querySelector(".nav__list");
+
+burgerBtnEl.addEventListener("click", function (e) {
+  const btn = e.target.closest(".burger-btn");
+  if (!btn) return;
+
+  shadeLayoutEl.classList.toggle("hidden");
+
+  if (getComputedStyle(burgerMenuEl).display === "none")
+    burgerMenuEl.style.setProperty("display", "flex");
+  else burgerMenuEl.style.setProperty("display", "none");
+});
+
+shadeLayoutEl.addEventListener("click", () => {
+  burgerMenuEl.style.setProperty("display", "none");
+  shadeLayoutEl.classList.add("hidden");
 });
